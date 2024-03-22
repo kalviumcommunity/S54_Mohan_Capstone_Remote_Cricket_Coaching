68b1e581f6e9a975a0d19254e4a4261be6bfbe87
mohankumar01012005 <kumar.mohan@kalvium.community>
2024-03-22 14:38:14 +0530

    updated

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Student_SignIn from './Components/Student_SignIn';
+import Student_SignUp from './Components/Student_SignUp';
+
+const App = () => {
+  return (
+    <div>
+      {/* <Student_SignIn></Student_SignIn> */}
+      <Student_SignUp></Student_SignUp>
+    </div>
+  );
+}
+
+export default App;
