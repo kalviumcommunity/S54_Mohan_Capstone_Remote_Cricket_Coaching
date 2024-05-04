65c38f88a1610857f9351db39dd28a8bfaa4033a
mohankumar01012005 <kumar.mohan@kalvium.community>
2024-05-04 13:48:24 +0530

    created a branch called coachBoard and created the components coachboard and and rendered the studentEligibility component inside that coachbord and now if a coach with particular id can post his available timings to his cluster directly

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
-// import Navigation from './Components/Navigation';
-// import StudentSignIn from './Components/Student/StudentSignIn';
-// import StudentSignUp from './Components/Student/StudentSignUp';
-// import CoachSignUp from './Components/Coach/CoachSignUp';
-// import CoachSignIn from './Components/Coach/CoachSignIn';
-// import CoachSubmitSuccess from './Components/CoachSubmitSuccess';
-// import CoachDataToStudent from './Components/Student/CoachDataToStudent';
+import Navigation from './Components/Navigation';
+import StudentSignIn from './Components/Student/StudentSignIn';
+import StudentSignUp from './Components/Student/StudentSignUp';
+import CoachSignUp from './Components/Coach/CoachSignUp';
+import CoachSignIn from './Components/Coach/CoachSignIn';
+import CoachSubmitSuccess from './Components/CoachSubmitSuccess';
+import CoachDataToStudent from './Components/Student/CoachDataToStudent';
 import StudentEligibility from './Components/Coach/StudentEligibility';
 import CoachBoard from '../src/Components/Coach/CoachBoard';
 
@@ -15,8 +15,8 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ChakraProvider><CoachBoard/></ChakraProvider>} />  
-        {/* <Route path="/" element={<ChakraProvider><Navigation /></ChakraProvider>} />
+        <Route path="/CoachBoard" element={<ChakraProvider><CoachBoard/></ChakraProvider>} />  
+        <Route path="/" element={<ChakraProvider><Navigation /></ChakraProvider>} />
         <Route path="/StudentSignUp" element={<ChakraProvider><StudentSignUp /></ChakraProvider>} />
         <Route path="/StudentSignIn" element={<ChakraProvider><StudentSignIn /></ChakraProvider>} />
         <Route path="/CoachSignUp" element={<ChakraProvider><CoachSignUp /></ChakraProvider>} />
@@ -25,7 +25,7 @@ const App = () => {
         <Route path="/StudentSignUp/CoachSubmitSuccess" element={<ChakraProvider><CoachSubmitSuccess /></ChakraProvider>} />
         <Route path="/coachDataToStudent" element={<ChakraProvider><CoachDataToStudent /></ChakraProvider>} />
 
-*/}
+
       <Route path="/studenteligibility" element={<StudentEligibility/>} />  
         {/* <Route path="/" element={<StudentEligibility />} /> */}
       </Routes>
